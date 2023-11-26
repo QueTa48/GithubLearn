2804410f827784f7186775ad7526a41ec1c179b5
LinuxAndrey <branoverv10@gmail.com>
2023-11-26 15:48:55 +0300

    Move definitions SomeClassA from .h to .cpp

--- SomeClassA.cpp
+++ SomeClassA.cpp
@@ -1,1 +1,16 @@
 #include "SomeClassA.h"
+
+SomeClassA::SomeClassA()
+{
+    _pData = new int(5);
+}
+
+SomeClassA::~SomeClassA()
+{
+    delete _pData;
+}
+
+void SomeClassA::printData()
+{
+    std::cout << *_pData << std::endl;
+}
